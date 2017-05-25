5755501b85cda4a43cce5c9785fbc7aad1f20be7
AfshaMohammed <mdafsha.7@gmail.com>
2017-05-24 21:48:30 -0500

    AR-Shopping-Mall
    create a plugin

---
+++ MySite/wp-content/plugins/MyPlugin/MyPlugin.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: MyPlugin
+Plugin URI: http://www.mainwp.com
+Description: This plugin does some stuff with WordPress
+Version: 1.0
+Author: Afsha Mohammed
+Author URI: http://www.mainwp.com
+License: GPL2
+
+
+
+*/
+?>
+
+
