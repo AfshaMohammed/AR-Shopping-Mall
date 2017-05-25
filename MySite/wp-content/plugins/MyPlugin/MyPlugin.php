b2606c2049a212a81b7b028718addc26b0003ffc
AfshaMohammed <mdafsha.7@gmail.com>
2017-05-25 10:37:18 -0500

    AR-Shopping-Mall
    add some code

--- MySite/wp-content/plugins/MyPlugin/MyPlugin.php
+++ MySite/wp-content/plugins/MyPlugin/MyPlugin.php
@@ -7,10 +7,17 @@ Version: 1.0
 Author: Afsha Mohammed
 Author URI: http://www.mainwp.com
 License: GPL2
-
-
-
 */
+
+add_action('admin_menu', 'MyPlugin_admin_actions');
+function MyPlugin_admin_actions(){
+	add_options_page('MyPlugin', 'MyPlugin','manage_options', FILE, 'MyPlugin_admin');	
+}
+function MyPlugin_admin()
+{
+	
+}
 ?>
 
 
+-S
